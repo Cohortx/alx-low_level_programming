30139c7502f39b9b5a794159adc9a947fdcf1eed
Cohortx <ibisomaxharrison@gmail.com>
2023-04-22 17:40:38 -0700

    1-last_digit.c

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - prints the last digit of the random
  * number stored in the variable n
- * Return: Always 0 (Success), 1 (failure)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d ", n, n % 10);
-	if (ld > 5)
+	if (n % 10 > 5)
 		printf("and is greater than 5\n");
-	else if (ld == 0)
+	else if (n % 10 == 0)
 		printf("and is 0\n");
-	else if (ld < 6 && n % 10 != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
-	return (0);		
+	return (0);
 }
