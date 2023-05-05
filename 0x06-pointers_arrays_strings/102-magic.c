e67470187636bf00a19bfc4b050e5cd4bde891f2
Cohortx <ibisomaxharrison@gmail.com>
2023-05-05 08:55:51 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*p = 98; /* added line of code */
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
