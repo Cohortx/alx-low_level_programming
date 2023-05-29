5f3905b826dd43963cd5e7f545d95bfb4f75d6f6
Cohortx <ibisomaxharrison@gmail.com>
2023-05-29 05:58:48 -0700

    committed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,37 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- *print_number - prints an integer
- *@n: number of the times table
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
  */
-
-void print_number(int n)
+int main(void)
 {
-	int divisor = 1;
-	int digit;
-	int negative = 0;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		negative = 1;
-	}
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
 
-	while (n / divisor > 9)
+	for (i = 0; i < 1024; ++i)
 	{
-		divisor *= 10;
+		if ((i % 3) == 0)
+		{
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
+		{
+			sum5 = sum5 + i;
+		}
 	}
-
-	while (divisor > 0)
-	{
-		digit = (n / divisor) % 10;
-		_putchar(digit + '0');
-		divisor /= 10;
-	}
-
-	if (negative)
-	{
-		n = -n;
-	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
