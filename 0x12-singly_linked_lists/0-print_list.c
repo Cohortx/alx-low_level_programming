c5e64c2cdb2b7e3f2e9aab9e78a7f74e8f32417f
Cohortx <ibisomaxharrison@gmail.com>
2023-06-01 15:43:46 -0700

    committed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,9 +3,9 @@
 
 /**
  * print_list - prints all the elements of a linked list
- * @h: pointer
+ * @h: pointer to the list_t list to print
  *
- * Return: number of nodes printed
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
